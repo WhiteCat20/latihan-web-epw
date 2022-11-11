289b39c19f3f44b9209730435b23f04eb2972d2c
WhiteCat20 <fzrahmadan@gmail.com>
2022-11-11 20:02:04 +0700

    Update main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,10 +7,10 @@
     <h1 class="text-center">Backend EPW 2023</h1>
     <h2 class="text-center">Hello {{ auth()->user()->username }}</h2>
     <div class="mt-5 d-flex justify-content-center">
-        <div>
-            <a href="/complete-data">
-                <h1>Lengkapi data anda!</h1>
-            </a>
-        </div>
+        @php
+            $time = time() * 2; // you have 1299446702 in time
+            $year = ($time / 31556926) % 12; // to get year
+            echo $year;
+        @endphp
     </div>
 @endsection
